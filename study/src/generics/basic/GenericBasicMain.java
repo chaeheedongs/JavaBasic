26cc352d76d6425e88d0372a7887e64229cd736d
chaeheedongs <qiip91@gmail.com>
2025-05-04 16:46:40 +0900

    generic | 기본 제네릭 타입 정리 / GenericBasicMain 코드에 타입소거 설명

---
+++ study/src/generics/basic/GenericBasicMain.java
@@ -0,0 +1,23 @@
+package generics.basic;
+
+import generics.basic.vo.Fruit;
+
+public class GenericBasicMain {
+
+    public static void main(String[] args) {
+
+        /**
+         * Generic은 컴파일(.java -> .class) 시점에 타입소거가 이루어진다.
+         *
+         * 타입소거(Type Erasure) / (Reifiable/non-Raifiable):
+         * -> 컴파일 시점에 비실체화 타입(제네릭)을 찾아 실체화 타입으로 변환해 준다.
+         *    이 과정에서 제네릭 타입이 모두 제거된다.
+         * */
+
+        Fruit<String> apple = new Fruit<>("apple");
+        System.out.println("apple = " + apple);
+
+        Fruit<String> banana = new Fruit<>("banana");
+        System.out.println("banana = " + banana);
+    }
+}
---
+++ study/src/generics/basic/vo/Fruit.java
@@ -0,0 +1,21 @@
+package generics.basic.vo;
+
+public class Fruit<T> {
+
+    private T name;
+
+    public Fruit(T name) {
+        this.name = name;
+    }
+
+    public T getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "name=" + name +
+                '}';
+    }
+}
